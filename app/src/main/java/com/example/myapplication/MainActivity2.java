8447d9756210047b069c84fdaab2ae9fc9cf7e00
StanFox1984 <stanislav.lisovskiy@gmail.com>
2022-10-25 19:36:07 +0300

    Added color highlighting and background

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,19 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity2 extends AppCompatActivity {
     public ExecutorService executorService;
@@ -24,10 +22,61 @@ public class MainActivity2 extends AppCompatActivity {
     public String server;
     public boolean register;
     public class InboxLoader implements UrlRunner.UrlResultCallback {
+        public class Message {
+            public String date;
+            public String from;
+            public String to;
+            public String message;
+            int message_start;
+            int message_end;
+        };
         @Override
         public void ResultCallback(String result_str) {
+            int index;
+            ArrayList<Message> messages = new ArrayList<Message>();
             TextView inbox = (TextView) findViewById(R.id.textView3);
-            inbox.setText(result_str);
+            index = result_str.indexOf("Date:");
+            while(index != -1) {
+                Message msg = new Message();
+                int to_index = result_str.indexOf("|To:", index);
+                if (to_index == -1)
+                    break;
+                msg.date = result_str.substring(index, to_index);
+                int from_index = result_str.indexOf("|From:", index);
+                if (from_index == -1)
+                    break;
+                msg.to = result_str.substring(to_index, from_index);
+                int message_index = result_str.indexOf("|Message:", index);
+                if (message_index == -1)
+                    break;
+                msg.from = result_str.substring(from_index, message_index);
+                index = result_str.indexOf("Date:", message_index);
+                int message_end = index == -1 ? result_str.length() : index;
+                msg.message = result_str.substring(message_index, message_end - 1);
+                messages.add(msg);
+            }
+            String final_string = "";
+
+            for (Message msg : messages)
+            {
+                final_string += msg.date.replace("|"," ");
+                final_string += msg.from.replace("|"," ");;
+                final_string += msg.to.replace("|"," ");;
+                final_string += "\n";
+                msg.message_start = final_string.length();
+                String message = msg.message.replace("|","").replace("Message:","");
+                message = message.replace("\"","");
+                final_string += message;
+                msg.message_end = final_string.length();
+                final_string += "\n";
+            }
+
+            SpannableString spannable = new SpannableString(final_string);
+            for(Message msg : messages)
+            {
+                spannable.setSpan(new ForegroundColorSpan(Color.YELLOW), msg.message_start, msg.message_end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+            inbox.setText(spannable);
         }
     }
     @Override
